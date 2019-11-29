7886959c30ff3921eef6beae0ac47d866a8035ae
craig fay <craigfay1@gmail.com>
2019-11-29 08:13:16 -0600

    Register Paddle component

--- src/pong.rs
+++ src/pong.rs
@@ -26,6 +26,63 @@ fn initialise_camera(world: &mut World) {
 
 impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        data.world.register::<Paddle>();
+        initialize_paddles(data.world).
         initialise_camera(data.world);
     }
+}
+
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    fn new(side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+// Implementing Component allows paddle to be attached to entities in the game
+impl Component for Paddle {
+    // When implemented the Component trait, we must specify the storage type
+    type Storage = DenseVecStorage<Self>;
+}
+
+/// Initializes one paddle on the left, and one paddle on the right
+fn initialize_paddles(world: &mut World) {
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // Correctly position the paddles
+    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // Create a left plank entity.
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    // Create right plank entity.
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
+        .build();
 }
