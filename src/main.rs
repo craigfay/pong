71c8fdd52fb556be170f7ddea663095afc861645
craig fay <craigfay1@gmail.com>
2019-11-29 12:20:56 -0600

    Add input bindings

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use amethyst::{
     prelude::*,
     core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
@@ -19,7 +20,11 @@ fn main () -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let assets_dir = app_root.join("assets");
 
+    let binding_path = app_root.join("config").join("bindings.ron");
     let display_config_path = app_root.join("config").join("display.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
 
     let mut world = World::new();
     let game_data = GameDataBuilder::default()
@@ -33,7 +38,7 @@ fn main () -> amethyst::Result<()> {
                 // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
                 .with_plugin(RenderFlat2D::default()),
         )?
-        // Add the transform bundle which handles tracking entity positions
+        .with_bundle(input_bundle)?
         .with_bundle(TransformBundle::new())?;
 
 
